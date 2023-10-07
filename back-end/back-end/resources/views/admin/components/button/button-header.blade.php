9ea8dabf37d33631a5cd204d176fea19b2af608a
le-dat-nnv <98152956+le-dat-nnv@users.noreply.github.com>
2023-10-07 20:06:29 +0700

    Delete back-end/back-end directory

--- back-end/back-end/resources/views/admin/components/button/button-header.blade.php
+++
@@ -1,2 +0,0 @@
-<a href="{{ route($urlbase.'create') }}" class="btn btn-sm bg-danger-light me-3 d-flex align-items-center">Thêm</a>
-<a href="{{ route($urlbase.'index') }}" class="btn btn-sm bg-success-light d-flex align-items-center">Danh sách</a>
