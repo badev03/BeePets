cd2f203e7b24f7f2ce2333a21aba5a85fd5c8df6
Vũ Anh Bá <145862750+badev03@users.noreply.github.com>
2023-10-07 19:21:21 +0700

    Revert "Feature/ledat/reviews ngày 10 /7 /2023 code update and api reviews and service"

--- resources/views/layouts/partials/script.blade.php
+++
@@ -1,8 +0,0 @@
-<script src="{{ asset('ckeditor5/ckeditor.js') }}"></script>
-<script>
-    ClassicEditor
-        .create( document.querySelector( '#editor' ) )
-        .catch( error => {
-        console.log(error)
-    } );
-</script>
