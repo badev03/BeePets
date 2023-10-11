e602ad287f5e917eef9526f273d65b56ec3f8beb
datkorea <datkidonguyen2003@gmail.com>
2023-10-11 13:53:33 +0700

    fix

--- front-end/src/api/appointmentApi.js
+++ front-end/src/api/appointmentApi.js
@@ -2,27 +2,27 @@ import instance from "./instance";
 
 const appointmentApi = {
     getAll(params) {
-        const url = "/appointments";
+        const url = "/appointmentss";
         return instance.get(url, { params });
     },
 
     get(id) {
-        const url = `/appointments/${id}`;
+        const url = `/appointmentss/${id}`;
         return instance.get(url);
     },
 
     add(data) {
-        const url = "/appointments";
+        const url = "/appointmentss";
         return instance.post(url, data);
     },
 
     update(data) {
-        const url = `/appointments/${data.id}`;
+        const url = `/appointmentss/${data.id}`;
         return instance.patch(url, data);
     },
 
     remove(id) {
-        const url = `/appointments/${id}`;
+        const url = `/appointmentss/${id}`;
         return instance.delete(url);
     },
 };
