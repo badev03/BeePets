2459b9caa5b5db0ca21371ee968b9f4ab2099cc8
le-dat-nnv <98152956+le-dat-nnv@users.noreply.github.com>
2023-10-11 09:54:11 +0700

    update code 11/10/2023 le huy dat

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -8,6 +8,7 @@ use \App\Http\Controllers\Api\ServiceController;
 use \App\Http\Controllers\Api\ReviewsController;
 use \App\Http\Controllers\Api\DoctorController;
 use App\Http\Controllers\Api\NewController;
+use App\Http\Controllers\Api\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -34,6 +35,7 @@ foreach ($objects as $key => $controller) {
 }
 Route::get('service-show', [ServiceController::class , 'showHome']);
 Route::get('service-filter', [ServiceController::class , 'filterService']);
+Route::post('service-filter-doctor', [ServiceController::class , 'filterDoctorService']);
 Route::get('new-post', [NewController::class , 'postNew']);
 Route::get('new-categories', [NewController::class , 'categoriesNew']);
 Route::get('new-search/{name}', [NewController::class , 'searchNew']);
@@ -42,3 +44,16 @@ Route::get('new-home', [NewController::class , 'showNew']);
 Route::post('/form', [BookingController::class, 'showForm']);
 Route::post('/save', [BookingController::class, 'save']);
 Route::post('/doctor/login', [DoctorController::class, 'login']);
+
+
+Route::post('send-opt-login' , [AuthController::class , 'sendOtp']);
+Route::post('check-phone' , [AuthController::class , 'checkerPhone'])->name('check-phone');
+Route::post('login-user' , [AuthController::class , 'LoginUserOtp']);
+Route::post('check-verify' , [AuthController::class , 'CheckVerify'])->name('check-verify');
+Route::post('check-verify-register' , [AuthController::class , 'CheckVerifyRegister']);
+Route::post('check-login' , [AuthController::class , 'CheckLogin']);
+Route::get('logout-user' , [AuthController::class , 'LogoutUser']);
+Route::post('register-user' , [AuthController::class , 'RegisterUser']);
+Route::post('forget-password' , [AuthController::class , 'ForgetPassWord']);
+Route::post('check-verify-forget-password' , [AuthController::class , 'CheckVerifyForgetPassword']);
+Route::post('reset-password' , [AuthController::class , 'ResetPassword']);
