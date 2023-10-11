59d9519303ea33b434974c0f8c8ec95efd5edf50
datkorea <datkidonguyen2003@gmail.com>
2023-10-11 11:47:20 +0700

    callApi

--- front-end/src/Component/Content.jsx
+++ front-end/src/Component/Content.jsx
@@ -1,93 +1,14 @@
 import Banner from "./Banner";
 import HomepageDoctors from "./Homepage/HomepageDoctors";
 import Homepage_Blog from "./Homepage/Homepage_blog";
+import Homepage_service from "./Homepage/Homepage_service";
 
 
 const Content = () => {
   return (
     <div>
       <Banner />
-      <section className="services-section-fourteen">
-        <div className="floating-bg">
-          <img src="../src/assets/img/bg/big-paw.png" alt="paw-image" />
-          <img src="../src/assets/img/bg/small-paw.png" alt="paw-image" />
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 aos" >
-              <div className="section-header-fourteen service-inner-fourteen">
-                <div className="service-inner-fourteen">
-                  <div className="service-inner-fourteen-two">
-                    <h3>DỊCH VỤ CỦA CHÚNG TÔI</h3>
-                  </div>
-                </div>
-                <h2>Chúng tôi có thể</h2>
-                <p>Dịch vụ chuyên nghiệp của chúng tôi</p>
-              </div>
-            </div>
-          </div>
-          <div className="row row-gap justify-content-center">
-            <div className="col-lg-3 col-md-4 col-sm-12 d-flex">
-              <a href="servicerdetails">
-                <div className="our-services-list w-100">
-                  <div className="service-icon">
-                    <img
-                      src="../src/assets/img/icons/injection.svg"
-                      alt="injection-icon"
-                    />
-                  </div>
-                  <h4>Tiêm chủng</h4>
-                  <p>
-                    Chúng ta cũng cần quan tâm tới vấn đề sức khỏe của thú cưng.
-                  </p>
-                </div>
-              </a>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-12 d-flex">
-              <div className="our-services-list w-100">
-                <div className="service-icon">
-                  <img
-                    src="../src/assets/img/icons/bottel.svg"
-                    alt="bottel-icon"
-                  />
-                </div>
-                <h4>Thuốc thú cưng</h4>
-                <p>
-                  Chúng ta cũng cần quan tâm tới vấn đề sức khỏe của thú cưng
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-12 d-flex">
-              <div className="our-services-list w-100">
-                <div className="service-icon">
-                  <img
-                    src="../src/assets/img/icons/bath-tub.svg"
-                    alt="pet-grooming-icon"
-                  />
-                </div>
-                <h4>Chăm sóc thú cưng</h4>
-                <p>
-                  Chúng ta cũng cần quan tâm tới vấn đề sức khỏe của thú cưng
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-12 d-flex">
-              <div className="our-services-list w-100">
-                <div className="service-icon">
-                  <img
-                    src="../src/assets/img/icons/pet-doctor.svg"
-                    alt="stethoscope-icon"
-                  />
-                </div>
-                <h4>Khám phá</h4>
-                <p>
-                  Chúng ta cũng cần quan tâm tới vấn đề sức khỏe của thú cưng
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Homepage_service />
 
       <HomepageDoctors />
 
--- front-end/src/Component/userDetail/User_BillDetail.jsx
+++ front-end/src/Component/userDetail/User_BillDetail.jsx
@@ -6,14 +6,14 @@ import userBillDetailApi from '../../api/userBillDetailApi';
 const User_BillDetail = () => {
     const { id } = useParams();
     // console.log(id);
-    const [bill, setBills] = useState(null);
+    const [bills, setBills] = useState(null);
 
     useEffect(() => {
         const fetchBlog = async () => {
             try {
                 const response = await userBillDetailApi.get(id);
                 setBills(response);
-                console.log(bill.id)
+                // console.log(bill.id)
             } catch (error) {
                 console.error("Không có dữ liệu:", error);
             }
@@ -22,7 +22,7 @@ const User_BillDetail = () => {
 
         fetchBlog();
     }, []);
-    if (!bill) {
+    if (!bills) {
         return <div>Loading...</div>;
     }
 
@@ -44,8 +44,8 @@ const User_BillDetail = () => {
                         </div>
                         <div className="col-12 col-md-6">
                             <div className="mb-3">
-                                <label className="mb-2">Mã hóa đơn: {bill.id}</label><br />
-                                <label className="mb-2">Ngày: {bill.date}</label>
+                                <label className="mb-2">Mã hóa đơn: {bills.id}</label><br />
+                                <label className="mb-2">Ngày: {bills.date}</label>
                             </div>
                         </div>
 
@@ -66,18 +66,18 @@ const User_BillDetail = () => {
 
 
                             <tbody >
-                                {/* {bill.map(bill => ( */}
-                                <tr key={bill.id}>
-                                    <td>{bill.bills.number}</td>
-                                    <td>{bill.bills.service_name}</td>
-                                    <td>{bill.bills.quantity}</td>
+                                {bills.bills.map(bill => (
+                                    <tr key={bill.id}>
+                                        <td>{bill.number}</td>
+                                        <td>{bill.service_name}</td>
+                                        <td>{bill.quantity}</td>
 
-                                    <td>{bill.bills.price} VNĐ</td>
+                                        <td>{bill.price} VNĐ</td>
 
-                                </tr>
+                                    </tr>
 
 
-                                {/* ))} */}
+                                ))}
 
                                 {/* lỗi vòng lặp map, hàm tổng tiền */}
 
