25c8286d1b485fe66a284cadf78a5c13adcead35
thieu12345hn <thieu12345hn@gmail.com>
2023-10-11 21:39:26 +0700

    feat/call-api-doctor

--- front-end/src/Component/DoctorProfile/DoctorProfile.jsx
+++ front-end/src/Component/DoctorProfile/DoctorProfile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import doctorsApi from '../../api/doctorsApi';
+import ReviewsDoctor from './ReviewsDoctor';
 const DoctorProfile = () => {
   const { id } = useParams();
   const [doctor, setDoctors] = useState(null);
@@ -10,7 +11,9 @@ const DoctorProfile = () => {
     const fetchBlog = async () => {
       try {
         const response = await doctorsApi.get(id);
-        setDoctors(response);
+        setDoctors(response.doctor);
+        // setDoctors(response.review);
+        console.log(response);
       } catch (error) {
         console.error("Không có dữ liệu:", error);
       }
@@ -124,9 +127,9 @@ const DoctorProfile = () => {
                 <div className="col-md-12 col-lg-9">
                   <div className="widget about-widget">
                     <h4 className="widget-title">Giới thiệu bản thân</h4>
-                    <p>{doctor.description.about}</p>
+                    <p>{doctor.description}</p>
                   </div>
-                  <div className="widget education-widget">
+                  {/* <div className="widget education-widget">
                     <h4 className="widget-title">Học vấn</h4>
                     <div className="experience-box">
                       <ul className="experience-list">
@@ -192,112 +195,13 @@ const DoctorProfile = () => {
                       
                       </ul>
                     </div>
-                  </div>
+                  </div> */}
                 
                 </div>
               </div>
             </div>
-        
-            <div role="tabpanel" id="doc_reviews" className="tab-pane fade">
-              <div className="widget review-listing">
-                <ul className="comments-list">
-                  <li>
-                    <div className="comment">
-                      <img className="avatar avatar-sm rounded-circle" alt="User Image" src="../src/assets/img/patients/patient.jpg" />
-                      <div className="comment-body">
-                        <div className="meta-data">
-                          <span className="comment-author">Lê Huy Đạt</span>
-                          <span className="comment-date">Đã đánh giá 2 ngày trước</span>
-                          <div className="review-count rating">
-                            <i className="fas fa-star filled" />
-                            <i className="fas fa-star filled" />
-                            <i className="fas fa-star filled" />
-                            <i className="fas fa-star filled" />
-                            <i className="fas fa-star" />
-                          </div>
-                        </div>
-                        <p className="recommended"><i className="far fa-thumbs-up" /> Tôi đánh giá cao bác sĩ này</p>
-                        <p className="comment-content">
-                        Điều quan trọng là phải tự chăm sóc nỗi đau, sau đó là sự trưởng thành của bệnh nhân, nhưng đồng thời cũng sẽ có rất nhiều công việc và nỗi đau. Để đi đến chi tiết nhỏ nhất, bài tập của chúng ta là gì? Tôi không nghĩ có thời gian để người hâm mộ trò chuyện
-                        </p>
-                        
-                      </div>
-                    </div>
-             
-                  </li>
-                  <li>
-                    <div className="comment">
-                      <img className="avatar avatar-sm rounded-circle" alt="User Image" src="../src/assets/img/patients/patient.jpg" />
-                      <div className="comment-body">
-                        <div className="meta-data">
-                          <span className="comment-author">Vũ Anh Bá</span>
-                          <span className="comment-date">Đã đánh giá 1 ngày trước</span>
-                          <div className="review-count rating">
-                            <i className="fas fa-star filled" />
-                            <i className="fas fa-star " />
-                            <i className="fas fa-star " />
-                            <i className="fas fa-star " />
-                            <i className="fas fa-star" />
-                          </div>
-                        </div>
-                        <p className="recommended" style={{color:"red"}}><i className="far fa-thumbs-up" /> Tôi đánh giá thấp bác sĩ này</p>
-                        <p className="comment-content">
-                        Bác sĩ này làm việc tệ vl, điều quan trọng là phải tự chăm sóc nỗi đau, sau đó là sự trưởng thành của bệnh nhân, nhưng đồng thời cũng sẽ có rất nhiều công việc và nỗi đau. Để đi đến chi tiết nhỏ nhất, bài tập của chúng ta là gì? Tôi không nghĩ có thời gian để người hâm mộ trò chuyện
-                        </p>
-                        
-                      </div>
-                    </div>
-             
-                  </li>
-                </ul>
-                
-              </div>
-              <div className="write-review">
-                <h4>Viết đánh giá cho <strong>Bác sĩ Tạ Anh Quí</strong></h4>
-                <form>
-                  <div className="mb-3">
-                
-                    <div className="star-rating">
-                      <input id="star-5" type="radio" name="rating" defaultValue="star-5" />
-                      <label htmlFor="star-5" title="5 stars">
-                        <i className="active fa fa-star" />
-                      </label>
-                      <input id="star-4" type="radio" name="rating" defaultValue="star-4" />
-                      <label htmlFor="star-4" title="4 stars">
-                        <i className="active fa fa-star" />
-                      </label>
-                      <input id="star-3" type="radio" name="rating" defaultValue="star-3" />
-                      <label htmlFor="star-3" title="3 stars">
-                        <i className="active fa fa-star" />
-                      </label>
-                      <input id="star-2" type="radio" name="rating" defaultValue="star-2" />
-                      <label htmlFor="star-2" title="2 stars">
-                        <i className="active fa fa-star" />
-                      </label>
-                      <input id="star-1" type="radio" name="rating" defaultValue="star-1" />
-                      <label htmlFor="star-1" title="1 star">
-                        <i className="active fa fa-star" />
-                      </label>
-                    </div>
-                  </div>
-                  <div className="mb-3">
-                    <label className="mb-2">Tiêu đề</label>
-                    <input className="form-control" type="text" placeholder="If you could say it in one sentence, what would you say?" />
-                  </div>
-                  <div className="mb-3">
-                    <label className="mb-2">Đánh giá của bạn</label>
-                    <textarea id="review_desc" maxLength={100} className="form-control" defaultValue={""} />
-                    {/* <div className="d-flex justify-content-between mt-3"><small className="text-muted"><span id="chars">100</span> characters remaining</small></div> */}
-                  </div>
-                  <hr />
-               
-                  <div className="submit-section">
-                    <button type="submit" className="btn btn-primary submit-btn">Đánh giá</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          
+        <ReviewsDoctor/>
+           
           </div>
         </div>
       </div>
