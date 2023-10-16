9367d9a3ec288077ac92bde0589d943f49134e33
le-dat-nnv <98152956+le-dat-nnv@users.noreply.github.com>
2023-10-16 20:39:09 +0700

    update code notification 16/10/2023

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -46,13 +46,13 @@ Route::get('new-search/{name}', [NewController::class, 'searchNew']);
 Route::get('new-home', [NewController::class, 'showNew']);
 
 
-Route::get('service-show', [ServiceController::class, 'showHome']);
-Route::get('service-filter', [ServiceController::class, 'filterService']);
-Route::post('service-filter-doctor', [ServiceController::class, 'filterDoctorService']);
-Route::get('new-post', [NewController::class, 'postNew']);
-Route::get('new-categories', [NewController::class, 'categoriesNew']);
-Route::get('new-search/{name}', [NewController::class, 'searchNew']);
-Route::get('new-home', [NewController::class, 'showNew']);
+//Route::get('service-show', [ServiceController::class, 'showHome']);
+//Route::get('service-filter', [ServiceController::class, 'filterService']);
+//Route::post('service-filter-doctor', [ServiceController::class, 'filterDoctorService']);
+//Route::get('new-post', [NewController::class, 'postNew']);
+//Route::get('new-categories', [NewController::class, 'categoriesNew']);
+//Route::get('new-search/{name}', [NewController::class, 'searchNew']);
+//Route::get('new-home', [NewController::class, 'showNew']);
 
 
 Route::post('/doctor/login', [DoctorController::class, 'login']);
