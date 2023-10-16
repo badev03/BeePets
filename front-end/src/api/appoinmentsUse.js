59d3bedf454b2bd8fee04e59526dc4b8b1bbb12d
thanh21062003 <thanh21062003@gmail.com>
2023-10-16 15:06:34 +0700

    feat/user_history

--- front-end/src/api/appoinmentsUse.js
+++ front-end/src/api/appoinmentsUse.js
@@ -1,10 +1,15 @@
 import instance from "./instance";
 
 const appointmentUsersApi = {
-    getAppoinments(params) {
-        const url = "/appointment-user";
-        return instance.get(url,  params );
-      },
+  getAppoinments(params) {
+    const url = "/appointment-user";
+    return instance.get(url, params);
+  },
+  getHistoryAppoinments(params) {
+    const url = "/history-user";
+    return instance.get(url, params);
+  },
 };
+
 
 export default appointmentUsersApi;
