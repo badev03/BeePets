f13a2971b94cc2de074f5fce91e55623af6f208c
datkorea <datkidonguyen2003@gmail.com>
2023-10-16 18:37:26 +0700

    fixlogout

--- front-end/src/api/logoutDoctor.js
+++ front-end/src/api/logoutDoctor.js
@@ -2,10 +2,14 @@ import instance from "./instance";
 
 const logoutDoctor = {
 
-    logout() {
-        const url = "/logout-doctor";
-        return instance.post(url);
+    logout(header) {
+        const url = "logout";
+        return instance.post(url, {}, header);
     },
+    // delete() {
+    //     const url = "/logout-doctor";
+    //     return instance.delete(url);
+    // },
 };
 
 export default logoutDoctor;
