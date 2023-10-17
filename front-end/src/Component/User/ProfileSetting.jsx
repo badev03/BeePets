38d08c1570f34aa6fe6bdc10e03b3213bb26ff4b
thieu12345hn <thieu12345hn@gmail.com>
2023-10-17 16:31:15 +0700

    feat/update-info-user

--- front-end/src/Component/User/ProfileSetting.jsx
+++ front-end/src/Component/User/ProfileSetting.jsx
@@ -3,19 +3,29 @@ import { Link } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import usersApi from '../../api/usersApi';
 import { useEffect, useState } from "react";
-
-
+import axios from 'axios';
+import { Alert } from 'antd';
 
 
 const ProfileSetting = () => {
 
   
-  const [user, setUser] = useState({ gender: 'Nam' });
+  const [user, setUser] = useState(
+    {
+      name: "",
+      address: "",
+      avatar:"",
+      // birthday: "",
+      phone: "",
+      email: "",
+      gender: "Nam",
+    }
+  );
 
   const token = localStorage.getItem('token');
-  const handleGenderChange = (event) => {
-    setUser({ ...user, gender: event.target.value });
-  };
+  // const handleGenderChange = (event) => {
+  //   setUser({ ...user, gender: event.target.value });
+  // };
 
   if (token) {
     useEffect(() => {
@@ -38,6 +48,73 @@ const ProfileSetting = () => {
       fetchUser();
     }, []);
   }
+  const [success, setSuccess] = useState(false);
+  const [errors, setErrors] = useState({});
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+  
+    // Validate name
+    if (!user.name) {
+      isValid = false;
+      errors["name"] = "Vui lòng nhập họ tên.";
+    }
+  
+    // Validate address
+    if (!user.address) {
+      isValid = false;
+      errors["address"] = "Vui lòng nhập địa chỉ.";
+    }
+  
+    // Validate phone
+    if (!user.phone) {
+      isValid = false;
+      errors["phone"] = "Vui lòng nhập số điện thoại.";
+    } else if (!/^\d{10}$/.test(user.phone)) {
+      isValid = false;
+      errors["phone"] = "Số điện thoại phải chứa chính xác 10 chữ số.";
+    }
+  
+    // Validate email
+    if (!user.email) {
+      isValid = false;
+      errors["email"] = "Vui lòng nhập email.";
+    } else if (!/\S+@\S+\.\S+/.test(user.email)) {
+      isValid = false;
+      errors["email"] = "Email không hợp lệ.";
+    }
+  
+    setErrors(errors);
+    return isValid;
+  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    // Call the API with user data to update the user information
+    if (validateForm()) {
+      // Call the API with user data to update the user information
+      try {
+        console.log(token);
+        const response = await axios.post(
+          `http://127.0.0.1:8000/api/save-infor-user`,
+          {
+            ...user,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+        }, 3000);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <div><div className="breadcrumb-bar-two">
       <div className="container">
@@ -59,20 +136,24 @@ const ProfileSetting = () => {
           <div className="row">
             <Sidebar />
             <div className="col-md-7 col-lg-8 col-xl-9">
-              <div className="card">
+        {success && <Alert message="Lưu thông tin tài khoản thành công" type="success" showIcon style={{marginBottom: "5px"}} />}
+        {Object.keys(errors).map((key, index) => (
+        <Alert key={index} message={errors[key]} type="error" showIcon style={{marginBottom: "5px"}} />
+      ))}
+              <div className="card" >
                 <div className="card-body">
-                  <form>
+                  <form onSubmit={handleFormSubmit} >
                     <div className="row">
                       <div className="col-12 col-md-12">
                         <div className="mb-3">
                           <div className="change-avatar">
                             <div className="profile-img">
-                              <img src="../src/assets/img/patients/patient.jpg" alt="User Image" />
+                              <img src={user.avatar} alt="User Image" />
                             </div>
                             <div className="upload-img">
                               <div className="change-photo-btn">
                                 <span><i className="fa fa-upload" />Tải ảnh lên</span>
-                                <input type="file" className="upload" />
+                                <input type="file" className="upload"  name="avatar" onChange={(e) => setUser({ ...user, avatar: e.target.value })}/>
                               </div>
 
                             </div>
@@ -82,58 +163,54 @@ const ProfileSetting = () => {
                       <div className="col-12 col-md-6">
                         <div className="mb-3">
                           <label className="mb-2">Họ Tên</label>
-                          <input type="text" className="form-control" value={user.name} />
+                          <input type="text" className="form-control" name="name" value={user.name}   onChange={(e) => setUser({ ...user, name: e.target.value })}/>
                         </div>
                       </div>
                       <div className="col-12 col-md-6">
                         <div className="mb-3">
                           <label className="mb-2">Địa chỉ</label>
-                          <input type="text" className="form-control" value={user.address} />
+                          <input type="text" className="form-control" name="address" value={user.address}   onChange={(e) => setUser({ ...user, address: e.target.value })} />
                         </div>
                       </div>
-                      <div className="col-12 col-md-6">
+                      {/* <div className="col-12 col-md-6">
                         <div className="mb-3">
                           <label className="mb-2">Ngày sinh</label>
                           <div className="cal-icon">
-                            <input type="text" className="form-control datetimepicker" value={user.birthday} />
+                            <input type="text" className="form-control datetimepicker" name="birthday" value={user.birthday} onChange={(e) => setUser({ ...user, birthday: e.target.value })} />
                           </div>
                         </div>
-                      </div>
+                      </div> */}
                       <div className="col-12 col-md-6">
                         <div className="mb-3">
                           <label className="mb-2">Số điện thoại</label>
-                          <input type="text" className="form-control" value={user.phone} />
+                          <input type="text" className="form-control" name="phone" value={user.phone} onChange={(e) => setUser({ ...user, phone: e.target.value })} />
                         </div>
                       </div>
                       <div className="col-12 col-md-6">
                         <div className="mb-3">
                           <label className="mb-2">Email</label>
-                          <input type="email" className="form-control" value={user.email} />
+                          <input type="text" className="form-control" name="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
                         </div>
                       </div>
                       <div className="col-md-6">
-      <div className="mb-3">
-        <label className="mb-2">Giới tính</label>
-        <select
-          className="form-select form-control"
-          value={user.gender}
-          onChange={handleGenderChange}
-        >
-          {user.gender && <option value={user.gender}>{user.gender}</option>}
-          {user.gender === 'Nam' ? (
-            <option value="Nữ">Nữ</option>
-          ) : (
-            <option value="Nam">Nam</option>
-          )}
-        </select>
-      </div>
-    </div>
-
-
-                    </div>
+                  <div className="mb-3">
+                    <label className="mb-2">Giới tính</label>
+                    <select
+                      className="form-select form-control"
+                      value={user.gender}
+                      onChange={(e) => setUser({ ...user, gender: e.target.value })}
+                      name="gender"
+                    >
+                        <option value="Nữ">Nữ</option>                   
+                        <option value="Nam">Nam</option>                 
+                    </select>
+                  </div>
+                  </div> 
+                  </div>
                     <div className="submit-section">
                       <button type="submit" className="btn btn-primary submit-btn">Lưu</button>
                     </div>
+                    
                   </form>
                 </div>
               </div>
